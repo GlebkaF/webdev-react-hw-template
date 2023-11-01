e56c50668aea920dffdd6fd7015323da747d2151
XshadowE <nimro-dell@mail.ru>
2023-11-01 20:14:04 +0700

    bar

---
+++ my-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Bar from "./components/Bar";
+import './App.css';
+import Menu from "./components/Menu";
+import Player from "./components/Player";
+
+function App() {
+  return (
+    <div> 
+        <Bar />
+        <Menu />
+        <Player />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Menu.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+function Menu(props) {
+return <div>menu</div>
+}
+
+export default Menu;
