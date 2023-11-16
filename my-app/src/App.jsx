7bf095c48fcd033e1844e1068562c09a4272accd
XshadowE <nimro-dell@mail.ru>
2023-11-16 13:22:25 +0700

    add skeleton

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,25 +1,21 @@
 import React from "react";
 import Bar from "./components/Bar";
-import './App.css';
+import "./App.css";
 import Menu from "./components/Menu";
 import Player from "./components/Player";
-import Sidebar from "./components/Sidebar";
+import SideBar from "./components/Sidebar/SideBar";
 import Search from "./components/Search";
 import Footer from "./components/footer";
 
-
 function App() {
   return (
-    <div> 
-       
-        <Menu />
-        <Search />
-        <Player />
-        <Sidebar />
-        <Bar />
-        <Footer />
-      
-        
+    <div>
+      <Menu />
+      <Search />
+      <Player />
+      <SideBar />
+      <Bar />
+      <Footer />
     </div>
   );
 }
