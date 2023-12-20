62e99286aa43f5653018350846ee39c5a14597ad
XshadowE <nimro-dell@mail.ru>
2023-12-20 14:35:17 +0700

    add Bar

--- my-app/src/components/Bar/Bar.jsx
+++ my-app/src/components/Bar/Bar.jsx
@@ -1,178 +1,89 @@
 import React from "react";
-import {styled} from "styled-components";
-
-const StyledBar = styled.div`
-position: absolute;
-bottom: 0;
-left: 0;
-width: 100%;
-background: rgba(28, 28, 28, 0.5);
-`
-
-const StyledBarContent = styled.div`
-display: -webkit-box;
-display: -ms-flexbox;
-display: flex;
--webkit-box-orient: vertical;
--webkit-box-direction: normal;
--ms-flex-direction: column;
-flex-direction: column;
-`
-
-const StyledBarPlayerProgress = styled.div`
-width: 100%;
-height: 5px;
-background: #2e2e2e;
-`
-
-const StyledBarPlayerBlock = styled.div`
-height: 73px;
-display: -webkit-box;
-display: -ms-flexbox;
-display: flex;
--webkit-box-orient: horizontal;
--webkit-box-direction: normal;
--ms-flex-direction: row;
-flex-direction: row;
--webkit-box-pack: justify;
--ms-flex-pack: justify;
-justify-content: space-between;
-`
-
-const StyledBarPlayer = styled.div`
-display: -webkit-box;
-display: -ms-flexbox;
-display: flex;
--webkit-box-orient: horizontal;
--webkit-box-direction: normal;
--ms-flex-direction: row;
-flex-direction: row;
--webkit-box-align: center;
--ms-flex-align: center;
-align-items: center;
--webkit-box-pack: start;
--ms-flex-pack: start;
-justify-content: flex-start;
-`
-
-const StyledBarPlayerControls = styled.div`
-display: -webkit-box;
-display: -ms-flexbox;
-display: flex;
--webkit-box-orient: horizontal;
--webkit-box-direction: normal;
--ms-flex-direction: row;
-flex-direction: row;
-padding: 0 27px 0 31px;
-`
-const BarButton = styled.div`
-padding: 5px;
-display: -webkit-box;
-display: -ms-flexbox;
-display: flex;
--webkit-box-align: center;
--ms-flex-align: center;
-align-items: center;
-`
-const StyledBarButtonPrev = styled(BarButton)`
-margin-right: 23px;
-`
-const StyledBarButtonPrevSvg = styled.svg`
-width: 15px;
-height: 14px;
-`
-
-const StyledBarButtonPlay = styled(BarButton)`
-margin-right: 23px;
-cursor: pointer;
-`
-
-
-
-//const StyledBar = styled.div``
+import * as S from "./Bar.styles";
 
 function Bar() {
-return (
-<StyledBar>
-          <StyledBarContent>
-            <StyledBarPlayerProgress></StyledBarPlayerProgress>
-            <StyledBarPlayerBlock>
-              <StyledBarPlayer>
-                <StyledBarPlayerControls>
-                  <StyledBarButtonPrev>
-                    <StyledBarButtonPrevSvg alt="prev">
-                      <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
-                    </StyledBarButtonPrevSvg>
-                  </StyledBarButtonPrev>
-                  <StyledBarButtonPlay >
-                    <svg className="player__btn-play-svg" alt="play">
-                      <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
-                    </svg>
-                  </StyledBarButtonPlay>
-                  <div className="player__btn-next">
-                    <svg className="player__btn-next-svg" alt="next">
-                      <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
-                    </svg>
-                  </div>
-                  <div className="player__btn-repeat _btn-icon">
-                    <svg className="player__btn-repeat-svg" alt="repeat">
-                      <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
-                    </svg>
-                  </div>
-                  <div className="player__btn-shuffle _btn-icon">
-                    <svg className="player__btn-shuffle-svg" alt="shuffle">
-                      <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
-                    </svg>
-                  </div>
-                </StyledBarPlayerControls>
+  return (
+    <S.Bar>
+      <S.BarContent>
+        <S.BarPlayerProgress></S.BarPlayerProgress>
+        <S.BarPlayerBlock>
+          <S.BarPlayer>
+            <S.BarPlayerControls>
+              <S.BarButtonPrev>
+                <S.BarButtonPrevSvg alt="prev">
+                  <use xlinkHref="img/icon/sprite.svg#icon-prev"></use>
+                </S.BarButtonPrevSvg>
+              </S.BarButtonPrev>
+              <S.BarButtonPlay>
+                <S.BarButtonPlaySvg alt="play">
+                  <use xlinkHref="img/icon/sprite.svg#icon-play"></use>
+                </S.BarButtonPlaySvg>
+              </S.BarButtonPlay>
+              <S.BarButtonNext>
+                <S.BarButtonNextSvg alt="next">
+                  <use xlinkHref="img/icon/sprite.svg#icon-next"></use>
+                </S.BarButtonNextSvg>
+              </S.BarButtonNext>
+              <S.BarButtonRepeat>
+                <S.BarButtonRepeatSvg alt="repeat">
+                  <use xlinkHref="img/icon/sprite.svg#icon-repeat"></use>
+                </S.BarButtonRepeatSvg>
+              </S.BarButtonRepeat>
+              <S.BarButtonShuffile>
+                <S.BarButtonShuffileSvg alt="shuffle">
+                  <use xlinkHref="img/icon/sprite.svg#icon-shuffle"></use>
+                </S.BarButtonShuffileSvg>
+              </S.BarButtonShuffile>
+            </S.BarPlayerControls>
 
-                <div className="player__track-play track-play">
-                  <div className="track-play__contain">
-                    <div className="track-play__image">
-                      <svg className="track-play__svg" alt="music">
-                        <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-                      </svg>
-                    </div>
-                    <div className="track-play__author">
-                      <a className="track-play__author-link" href="http://">Ты та...</a>
-                    </div>
-                    <div className="track-play__album">
-                      <a className="track-play__album-link" href="http://">Баста</a>
-                    </div>
-                  </div>
+            <S.BarPlayerTrackPlay>
+              <S.BarTrackPlayContain>
+                <S.BarTrackPlayImage>
+                  <S.BarTrackPlayImageSvg alt="music">
+                    <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+                  </S.BarTrackPlayImageSvg>
+                </S.BarTrackPlayImage>
+                <S.BarTrackPlayAuthor>
+                  <S.BarTrackPlayAuthorLink href="http://">
+                    Ты та...
+                  </S.BarTrackPlayAuthorLink>
+                </S.BarTrackPlayAuthor>
+                <S.BarTrackPlayAlbum>
+                  <S.BarTrackPlayAlbumLink href="http://">
+                    Баста
+                  </S.BarTrackPlayAlbumLink>
+                </S.BarTrackPlayAlbum>
+              </S.BarTrackPlayContain>
 
-                  <div className="track-play__like-dis">
-                    <div className="track-play__like _btn-icon">
-                      <svg className="track-play__like-svg" alt="like">
-                        <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-                      </svg>
-                    </div>
-                    <div className="track-play__dislike _btn-icon">
-                      <svg className="track-play__dislike-svg" alt="dislike">
-                        <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-              </StyledBarPlayer>
-              <div className="bar__volume-block volume">
-                <div className="volume__content">
-                  <div className="volume__image">
-                    <svg className="volume__svg" alt="volume">
-                      <use xlinkHref="/img/icon/sprite.svg#icon-volume"></use>
-                    </svg>
-                  </div>
-                  <div className="volume__progress _btn">
-                    <input
-                      className="volume__progress-line _btn"
-                      type="range"
-                      name="range"/>
-                  </div>
-                </div>
-              </div>
-            </StyledBarPlayerBlock>
-          </StyledBarContent>
-        </StyledBar>
-        )
-        }
+              <S.BarTrackPlayLikeDis>
+                <S.BarTrackPlay>
+                  <S.BarTrackPlayLikeSvg alt="like">
+                    <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+                  </S.BarTrackPlayLikeSvg>
+                </S.BarTrackPlay>
+                <S.BarTrackPlayDislike>
+                  <S.BarTrackPlayDislikeSvg alt="dislike">
+                    <use xlinkHref="img/icon/sprite.svg#icon-dislike"></use>
+                  </S.BarTrackPlayDislikeSvg>
+                </S.BarTrackPlayDislike>
+              </S.BarTrackPlayLikeDis>
+            </S.BarPlayerTrackPlay>
+          </S.BarPlayer>
+          <S.BarVolumeBlockVolume>
+            <S.BarVolumeContent>
+              <S.BarVolumeImage>
+                <S.BarVolumeSvg alt="volume">
+                  <use xlinkHref="/img/icon/sprite.svg#icon-volume"></use>
+                </S.BarVolumeSvg>
+              </S.BarVolumeImage>
+              <S.BarVolumeProgress>
+                <S.BarVolumeProgressLine type="range" name="range" />
+              </S.BarVolumeProgress>
+            </S.BarVolumeContent>
+          </S.BarVolumeBlockVolume>
+        </S.BarPlayerBlock>
+      </S.BarContent>
+    </S.Bar>
+  );
+}
 export default Bar;
