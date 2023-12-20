31e1b4dfba09d6aa4acda37745523b28823bf154
XshadowE <nimro-dell@mail.ru>
2023-12-20 15:37:54 +0700

    add search

--- my-app/src/components/Search/Search.jsx
+++ my-app/src/components/Search/Search.jsx
@@ -1,23 +1,17 @@
 import React from "react";
+import * as S from "./Search.styles";
 
-
-
-function Search () {
-    return (
-        <div className="main__centerblock centerblock">
-        <div className="centerblock__search search">
-        <svg className="search__svg">
+function Search() {
+  return (
+    <S.MainCenterblock>
+      <S.CenterblockSearch>
+        <S.SearchSvg>
           <use xlinkHref="img/icon/sprite.svg#icon-search"></use>
-        </svg>
-        <input
-          className="search__text"
-          type="search"
-          placeholder="Поиск"
-          name="search"/>
-      </div>
-      <h2 className="centerblock__h2">Треки</h2>
-     
-      </div>
-    )
+        </S.SearchSvg>
+        <S.SearchText type="search" placeholder="Поиск" name="search" />
+      </S.CenterblockSearch>
+      <S.MainCenterblockH2>Треки</S.MainCenterblockH2>
+    </S.MainCenterblock>
+  );
 }
 export default Search;
